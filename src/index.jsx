97c0de9e5dd5437689a7eaaeb20a4b84d897ee66
megait004 <giapfc123@gmail.com>
2026-03-26 11:11:26 +0700

    Add @vercel/analytics package and integrate analytics injection in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 import '@/assets/css/bootstrap.min.css';
 import '@/assets/css/style.css';
 import { RouterProvider } from 'react-router';
 import router from '@/router/router';
+
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
